3606619946bbc8233097fb071f9796c4d4acf311
Maxwell <maxwell.fitzpatrick117@gmail.com>
2026-02-11 13:23:08 -0800

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -4,37 +4,13 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-using std::endl;
-using std::cin;
-using std::cout;
-
 int main()
 {
-<<<<<<< HEAD
-  cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-  cout << "Hi, please enter two whole numbers: ";
-=======
     cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
     cout << "Hi, please enter two whole numbers: ";
->>>>>>> main
 
   int x,y;
 
-<<<<<<< HEAD
-  cin >> x >> y;
-  cout << "Addition: " << x + y << endl;
-  cout << "Subtraction: " << x - y << endl;
-  cout << "Multiplication: " << x * y << endl;
-  if ( y== 0 ) {
-    cout << "Divding by zero is niot a number\n";
-  }
-  else { 
-    cout << "Division: " << x / y << endl;
-  }
-  cout << "Remainder: " << x % y << endl;
-  cout << "Square Root: " << sqrt(x) << endl;
-  cout << "Square: " << pow(x, y) << endl;
-=======
     cin >> x >> y;
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
@@ -43,7 +19,6 @@ int main()
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
->>>>>>> main
 
   return 0;
 }
